6bcb9704cc870daf7e2a3bb0bcb21d82d873c652
RIGAT Pierre-André <pierre-andre.rigat@etu.univ-amu.fr>
2024-06-03 10:49:01 +0200

    ChessApplication.java and chess-view.fxml added

---
+++ src/main/java/fr/iut/SAEChess/ChessApplication.java
@@ -0,0 +1,16 @@
+package fr.iut.SAEChess;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ChessApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
