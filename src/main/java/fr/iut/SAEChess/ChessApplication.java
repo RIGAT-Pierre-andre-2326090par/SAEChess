789ac747c49c46a323cc315db6e96bd1c417d0f9
RIGAT Pierre-André <pierre-andre.rigat@etu.univ-amu.fr>
2024-06-03 11:00:43 +0200

    ChessApplication.java load and launch chess-view.fxml

--- src/main/java/fr/iut/SAEChess/ChessApplication.java
+++ src/main/java/fr/iut/SAEChess/ChessApplication.java
@@ -1,7 +1,12 @@
 package fr.iut.SAEChess;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.saechess.HelloApplication;
+
+import java.io.IOException;
 
 public class ChessApplication extends Application {
 
@@ -10,7 +15,11 @@ public class ChessApplication extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        primaryStage.setTitle("Hello!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
