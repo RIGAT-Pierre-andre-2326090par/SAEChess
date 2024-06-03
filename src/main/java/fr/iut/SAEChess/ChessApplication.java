0e39151d4e536555ec3943053eda69c9efb87dbd
Vernagut <titouanvernagut@gmail.com>
2024-06-03 12:04:51 +0200

    graphique

--- src/main/java/fr/iut/SAEChess/ChessApplication.java
+++ src/main/java/fr/iut/SAEChess/ChessApplication.java
@@ -4,22 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.example.saechess.HelloApplication;
 
 import java.io.IOException;
 
 public class ChessApplication extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        primaryStage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(ChessApplication.class.getResource("chess-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        primaryStage.setTitle("Chess");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
