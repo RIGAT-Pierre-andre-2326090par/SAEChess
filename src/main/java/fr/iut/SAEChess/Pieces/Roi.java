2b9e04ed0119d098aace07bff4f4c00e4b0808f4
RIGAT Pierre-André <pierre-andre.rigat@etu.univ-amu.fr>
2024-06-04 14:46:02 +0200

    pion et roi fini

--- src/main/java/fr/iut/SAEChess/Pieces/Roi.java
+++ src/main/java/fr/iut/SAEChess/Pieces/Roi.java
@@ -13,11 +13,20 @@ public class Roi extends ChessPiece {
 
     @Override
     public boolean isValidMove(int x, int y, ChessBoard board) {
-        return false;
+        if (x < 0 || x >= 8 || y < 0 || y >= 8) return false;
+        else if (Math.abs(x - getX()) <= 1 && Math.abs(y - getY()) <= 1 && (Math.abs(x - getX()) + Math.abs(y - getY()) != 0)) return true;
+        else return false;
     }
 
     @Override
     public int[][] imagineAllMoves(ChessBoard board) {
-        return new int[0][];
+        int[][] moves = new int[4][];
+        int ind = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (isValidMove(i, j, board)) moves[ind++] = new int[]{i, j};
+            }
+        }
+        return moves;
     }
 }
