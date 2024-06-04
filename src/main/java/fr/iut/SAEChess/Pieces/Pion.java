88f61cc7b5464a01084ff2a4bc5c3477180f0348
RIGAT Pierre-André <pierre-andre.rigat@etu.univ-amu.fr>
2024-06-04 14:46:02 +0200

    pion et roi fini

--- src/main/java/fr/iut/SAEChess/Pieces/Pion.java
+++ src/main/java/fr/iut/SAEChess/Pieces/Pion.java
@@ -3,6 +3,8 @@ package fr.iut.SAEChess.Pieces;
 import fr.iut.SAEChess.ChessBoard;
 import fr.iut.SAEChess.ChessPiece;
 
+import static java.lang.Math.abs;
+
 public class Pion extends ChessPiece {
 
     public Pion(boolean blanc, int x, int y) {
@@ -13,13 +15,27 @@ public class Pion extends ChessPiece {
 
     @Override
     public boolean isValidMove(int x, int y, ChessBoard board) {
-        return false;
+        if (x < 0 || x >= 8 || y < 0 || y >= 8) return false;
+        else if (isBlanc()) {
+            if (getX() == 6 && x - getX() == -2 && x - getX() == -1 && y - getY() == 0) return true;
+            else if (x - getX() == -1 && y - getY() == 0) return true;
+            else if (!board.get(x, y).isBlanc()) return x - getX() == -1 && abs(y - getY()) <= 1;
+        } else {
+            if (getX() == 1 && x - getX() == 2 && x - getX() == 1 && y - getY() == 0) return true;
+            else if (x - getX() == 1 && y - getY() == 0) return true;
+            else if (!board.get(x, y).isBlanc()) return x - getX() == 1 && abs(y - getY()) <= 1;
+        } return false;
     }
 
     @Override
     public int[][] imagineAllMoves(ChessBoard board) {
-        return new int[0][];
+        int[][] moves = new int[4][];
+        int ind = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (isValidMove(i, j, board)) moves[ind++] = new int[]{i, j};
+            }
+        }
+        return moves;
     }
-
-
 }
