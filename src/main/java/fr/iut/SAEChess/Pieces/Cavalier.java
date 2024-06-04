c4a2e1b34b56864392c798e084cddd1db1410605
RIGAT Pierre-André <pierre-andre.rigat@etu.univ-amu.fr>
2024-06-04 15:22:09 +0200

    cavalier et roi fini

--- src/main/java/fr/iut/SAEChess/Pieces/Cavalier.java
+++ src/main/java/fr/iut/SAEChess/Pieces/Cavalier.java
@@ -12,11 +12,21 @@ public class Cavalier extends ChessPiece {
 
     @Override
     public boolean isValidMove(int x, int y, ChessBoard board) {
-        return false;
+        if (x < 0 || x >= 8 || y < 0 || y >= 8) return false;
+        else if (board.get(x, y) == null || board.get(x, y).isBlanc() != isBlanc())
+            return (Math.abs(x - getX()) == 3 && Math.abs(y - getY()) == 1) || (Math.abs(x - getX()) == 1 && Math.abs(y - getY()) == 3);
+        else return false;
     }
 
     @Override
     public int[][] imagineAllMoves(ChessBoard board) {
-        return new int[0][];
+        int[][] moves = new int[4][];
+        int ind = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (isValidMove(i, j, board)) moves[ind++] = new int[]{i, j};
+            }
+        }
+        return moves;
     }
 }
--- src/main/java/fr/iut/SAEChess/Pieces/Roi.java
+++ src/main/java/fr/iut/SAEChess/Pieces/Roi.java
@@ -14,8 +14,9 @@ public class Roi extends ChessPiece {
     @Override
     public boolean isValidMove(int x, int y, ChessBoard board) {
         if (x < 0 || x >= 8 || y < 0 || y >= 8) return false;
-        else if (Math.abs(x - getX()) <= 1 && Math.abs(y - getY()) <= 1 && (Math.abs(x - getX()) + Math.abs(y - getY()) != 0)) return true;
-        else return false;
+        else if (board.get(x, y) == null || board.get(x, y).isBlanc() != isBlanc())
+            if (Math.abs(x - getX()) <= 1 && Math.abs(y - getY()) <= 1 && (Math.abs(x - getX()) + Math.abs(y - getY()) != 0)) return true;
+        return false;
     }
 
     @Override
