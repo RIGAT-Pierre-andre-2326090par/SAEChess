7a0476e97542f5541228669a9ae7272bd295e991
RIGAT Pierre-André <pierre-andre.rigat@etu.univ-amu.fr>
2024-06-06 15:14:14 +0200

    affichage des pièces réglée

--- src/main/java/fr/iut/SAEChess/Pieces/Pion.java
+++ src/main/java/fr/iut/SAEChess/Pieces/Pion.java
@@ -17,15 +17,15 @@ public class Pion extends ChessPiece {
     public boolean isValidMove(int x, int y, ChessBoard board) {
         System.out.println(x - getX());
         System.out.println(y - getY());
-        if (x < 0 || x >= 8 || y < 0 || y >= 8) return false;
+        if (y < 0 || y >= 8 || x < 0 || x >= 8) return false;
         else if (isBlanc()) {
-            if (getX() == 6 && (x - getX() == -2 || x - getX() == -1) && y - getY() == 0) return true;
-            else if (x - getX() == -1 && y - getY() == 0) return true;
-            else if (board.get(x, y) != null && board.get(x, y).isBlanc()) return x - getX() == 1 && abs(y - getY()) <= 1;
+            if (getY() == 6 && (y - getY() == 2 || y - getY() == 1) && x - getX() == 0) return true;
+            else if (y - getY() == 1 && x - getX() == 0) return true;
+            else if (board.get(x, y) != null && board.get(x, y).isBlanc() && board.get(x, y) != this) return abs(y - getY()) <= 1 && abs(x - getX()) == 1;
         } else {
-            if (getX() == 1 && (x - getX() == 2 || x - getX() == 1) && y - getY() == 0) return true;
-            else if (x - getX() == 1 && y - getY() == 0) return true;
-            else if (board.get(x, y) != null && !board.get(x, y).isBlanc()) return x - getX() == -1 && abs(y - getY()) <= 1;
+            if (getY() == 1 && (abs(y - getY()) == 2 || abs(y - getY()) == 1) && x - getX() == 0) return true;
+            else if (y - getY() == 1 && x - getX() == 0) return true;
+            else if (board.get(x, y) != null && !board.get(x, y).isBlanc() && board.get(x, y) != this) return abs(y - getY()) <= 1 && abs(x - getX()) == 1;
         } return false;
     }
 
