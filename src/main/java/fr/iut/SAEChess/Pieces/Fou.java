e1812b25812ea302cdacda0c962bca22587cf55e
Pierre-Andre <pierreandre.rigat@etu.univ-amu.fr>
2024-06-07 13:32:29 +0200

    check déplacement avance

--- src/main/java/fr/iut/SAEChess/Pieces/Fou.java
+++ src/main/java/fr/iut/SAEChess/Pieces/Fou.java
@@ -13,10 +13,22 @@ public class Fou extends ChessPiece {
 
     @Override
     public boolean isValidMove(int x, int y, ChessBoard board) {
-        if (x < 0 || x >= 8 || y < 0 || y >= 8 && (x == getX() && y == getY())) return false;
+        if (x < 0 || x >= 8 || y < 0 || y >= 8 || (x == getX() && y == getY())) return false;
         int deltaX = x - this.getX();
         int deltaY = y - this.getY();
         if (Math.abs(deltaX) == Math.abs(deltaY) && deltaX != 0 && deltaY != 0) {
+            for (int nb = x; nb < getX(); nb++) {
+                if (board.get(nb, nb) != null && board.get(nb, nb).isBlanc() == isBlanc()) return false;
+            }
+            for (int nb = x; nb < getX(); nb++) {
+                if (board.get(nb, -nb) != null && board.get(nb, -nb).isBlanc() == isBlanc()) return false;
+            }
+            for (int nb = x; nb < getX(); nb++) {
+                if (board.get(-nb, nb) != null && board.get(-nb, nb).isBlanc() == isBlanc()) return false;
+            }
+            for (int nb = x; nb < getX(); nb++) {
+                if (board.get(-nb, -nb) != null && board.get(-nb, -nb).isBlanc() == isBlanc()) return false;
+            }
             return true;
         }
         return false;
