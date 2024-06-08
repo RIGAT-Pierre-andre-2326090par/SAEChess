93b411fe0ab2ce80a2b401f896812288b3bb8ae5
RIGAT Pierre-André <pierre-andre.rigat@etu.univ-amu.fr>
2024-06-08 14:56:56 +0200

    tout les déplacement fonctionnel

--- src/main/java/fr/iut/SAEChess/ChessBot.java
+++ src/main/java/fr/iut/SAEChess/ChessBot.java
@@ -7,18 +7,15 @@ import java.util.Random;
 public class ChessBot {
     private final Random random = new Random();
 
-    // Get a random move from all legal moves for the bot
     public int[] getMove(ChessBoard board, boolean isWhite) {
         List<int[]> legalMoves = getAllLegalMoves(board, isWhite);
         if (legalMoves.isEmpty()) {
             return null; // No legal moves available
         }
 
-        // For simplicity, the bot just picks a random legal move
         return legalMoves.get(random.nextInt(legalMoves.size()));
     }
 
-    // Get all legal moves for the bot
     private List<int[]> getAllLegalMoves(ChessBoard board, boolean isWhite) {
         List<int[]> legalMoves = new ArrayList<>();
         for (int i = 0; i < board.getBoardSize(); i++) {
--- src/main/java/fr/iut/SAEChess/Pieces/Reine.java
+++ src/main/java/fr/iut/SAEChess/Pieces/Reine.java
@@ -6,7 +6,7 @@ import fr.iut.SAEChess.ChessPiece;
 public class Reine extends ChessPiece {
 
     public Reine(boolean blanc, int x, int y) {
-        super(blanc, "", x, y, 9);
+        super(blanc, "", x, y, 3);
         if (blanc) setImg("reineB.png");
         else setImg("reineN.png");
     }
@@ -20,6 +20,57 @@ public class Reine extends ChessPiece {
         int deltaX = x - this.getX();
         int deltaY = y - this.getY();
         if (x == getX() && y == getY()) return false;
-        return (Math.abs(deltaX) == Math.abs(deltaY) || y == getY() || x == getX());
+        else if ((y == getY())) {
+            if (getX() < x) {
+                for (int nb = getX() + 1; nb < x; nb++)
+                    if (board.get(nb, y) != null) return false;
+            } else {
+                for (int nb = getX() - 1; nb > x; nb--)
+                    if (board.get(nb, y) != null) return false;
+            }
+            return true;
+        } else if (((x) == getX())) {
+            if (getY() < y) {
+                for (int nb = getY() + 1; nb < y; nb++)
+                    if (board.get(x, nb) != null) return false;
+            } else {
+                for (int nb = getY() - 1; nb > y; nb--)
+                    if (board.get(x, nb) != null) return false;
+            }
+            return true;
+        }
+        else if (Math.abs(deltaX) == Math.abs(deltaY)) {
+            if (x > getX() && y > getY()) {
+                int j = getY() + 1;
+                for (int i = getX() + 1; i < x && j < y; i++) {
+                    if (board.get(i, j) != null) return false;
+                    j++;
+                }
+                return true;
+            } else if (x > getX() && y < getY()) {
+                int j = getY() - 1;
+                for (int i = getX() + 1; i < x && j > y; i++) {
+                    if (board.get(i, j) != null) return false;
+                    j--;
+                }
+                return true;
+            } else if (x < getX() && y > getY()) {
+                int j = getY() + 1;
+                for (int i = getX() - 1; i > x && j < y; i--) {
+                    if (board.get(i, j) != null) return false;
+                    j++;
+                }
+                return true;
+            } else if (x < getX() && y < getY()) {
+                int j = getY() - 1;
+                for (int i = getX() - 1; i > x && j > y; i--) {
+                    if (board.get(i, j) != null) return false;
+                    j--;
+                }
+                return true;
+            }
+            return (Math.abs(deltaX) == Math.abs(deltaY) || y == getY() || x == getX());
+        }
+        return false;
     }
 }
